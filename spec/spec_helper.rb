308d59f8a7b01f78b3a9fa5b90ea1ee479ba05a5
Evan Clough & Terry Marr <student@epicodus.com>
2015-08-26 11:50:55 -0700

    Define class associations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,15 @@ require 'capybara/rspec'
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 require './app'
+
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Recipe.all().each() do |recipe|
+      recipe.destroy()
+    end
+    Ingredient.all().each() do |ingr|
+      ingr.destroy()
+    end
+  end
+end
