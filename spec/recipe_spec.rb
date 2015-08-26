323b7f2e85ff749f35dd5eaf06def8d11e953881
Evan Clough & Terry Marr <student@epicodus.com>
2015-08-26 12:02:38 -0700

    Define recipe to tag many to many relationship

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -7,4 +7,19 @@ describe(Recipe) do
     recipe.ingredients.push(ingredient)
     expect(recipe.ingredients).to(eq([ingredient]))
   end
+
+  it('tells you its tags') do
+    recipe = Recipe.create({name: "Austin's diet"})
+    tag1 = Tag.create({tag: "breakfast"})
+    tag2 = Tag.create({tag: "lunch"})
+    tag3 = Tag.create({tag: "dinner"})
+    tag4 = Tag.create({tag: "snack"})
+    tag5 = Tag.create({tag: "holiday"})
+
+    Tag.all.each do |tag|
+      recipe.tags.push(tag)
+    end
+
+    expect(recipe.tags).to(eq(Tag.all))
+  end
 end
